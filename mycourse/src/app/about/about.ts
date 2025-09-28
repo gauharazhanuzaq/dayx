2bcc54cf75f8e942e9aa8a0adfa050c9e0ede50f
Gauhar Zhanuzak <gauharzhanuzak@MacBook-Air-Gauhar.local>
2025-09-28 19:52:16 +0500

    angular about

--- mycourse/src/app/about/about.ts
+++ mycourse/src/app/about/about.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './about.html',
   styleUrl: './about.css'
 })
 export class About {
+  title = "I'm Gauhar";
+  subtitle = "I'm UI/UX Designer working in ** company";
+  passion = "I’m passionate about front-end development, design, and creating modern web apps.";
+  hobbies = "I enjoy experimenting with new technologies and building projects that combine creativity with functionality.";
 
+  photoUrl = 'assets/profile.jpg';
+
+  likes = 0;
+  showMessage = false;
+
+  addLike() {
+    this.likes++;
+  }
+  toggleMessage() {
+    this.showMessage = !this.showMessage;
+  }
+  name = '';
+  email = '';
+  subscribed = false;
+  subscribe() {
+    if (this.email) {
+      this.subscribed = true;
+    }
+  }
 }

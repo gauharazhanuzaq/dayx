066d701d416ac27485b75f3bcf1d93dd2a8ead5c
gzhanuzak <gzhanuzak@dmcorp.kz>
2025-10-14 10:27:04 +0500

    todo2

--- mycourse/src/app/services/todo.ts
+++ mycourse/src/app/services/todo.ts
@@ -2,15 +2,19 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class TodoService {
-  private apiUrl = 'https://jsonplaceholder.typicode.com/todos?_limit=10';
+  private apiUrl = 'https://jsonplaceholder.typicode.com/todos';
 
   constructor(private http: HttpClient) {}
 
-  getTodos(): Observable<any[]> {
-    return this.http.get<any[]>(this.apiUrl);
+  getTodos(limit?: number): Observable<any[]> {
+    let url = this.apiUrl;
+    if (limit) url += `?_limit=${limit}`;
+    return this.http.get<any[]>(url);
+  }
+
+  searchTodos(query: string, limit: number = 15): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}?_limit=${limit}`);
   }
 }
--- mycourse/src/app/todo-list/todo-list.ts
+++ mycourse/src/app/todo-list/todo-list.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TodoService } from '../services/todo';
+import { Subject, Subscription } from 'rxjs';
+import { debounceTime, switchMap, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-todo-list',
@@ -9,15 +11,44 @@ import { TodoService } from '../services/todo';
   templateUrl: './todo-list.html',
   styleUrls: ['./todo-list.css']
 })
-export class TodoListComponent {
+export class TodoListComponent implements OnInit, OnDestroy {
   todos: any[] = [];
+  limit: number = 15;
   isLoading = false;
+  searchTerm = '';
+  private searchSubject = new Subject<string>();
+  private subscription = new Subscription();
 
   constructor(private todoService: TodoService) {}
 
+  ngOnInit() {
+    this.loadTodos();
+
+    const searchSub = this.searchSubject.pipe(
+      debounceTime(400),
+      switchMap(term => {
+        this.isLoading = true;
+        return this.todoService.searchTodos(term, this.limit).pipe(
+          map(todos => {
+            if (term.trim() === '') return todos;
+            return todos.filter(t => t.title.toLowerCase().includes(term.toLowerCase()));
+          })
+        );
+      })
+    ).subscribe({
+      next: (data) => {
+        this.todos = data;
+        this.isLoading = false;
+      },
+      error: () => this.isLoading = false
+    });
+
+    this.subscription.add(searchSub);
+  }
+
   loadTodos() {
     this.isLoading = true;
-    this.todoService.getTodos().subscribe({
+    this.todoService.getTodos(this.limit).subscribe({
       next: (data) => {
         this.todos = data;
         this.isLoading = false;
@@ -27,5 +58,14 @@ export class TodoListComponent {
         this.isLoading = false;
       }
     });
+  }
+
+  onSearch(term: string) {
+    this.searchTerm = term;
+    this.searchSubject.next(term);
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
